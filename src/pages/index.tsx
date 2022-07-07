53511842e641b87034c62e4a59f97a80cee5fab2
Bruno Henrique <brunohenrique242@gmail.com>
2022-07-07 15:36:46 -0300

    Inciando a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,21 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1 >
-        Hello <span>World</span>
-      </h1>
+      <main>
+        <section>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> World.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
 
     </>
   )
